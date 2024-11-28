d5e40320c336f04c03c669b11223912151daea42
AgentitoE <110563900+agentitoe@users.noreply.github.com>
2024-11-28 17:34:59 +0100

    Create prank.bat

---
+++ prank.bat
@@ -0,0 +1,6 @@
+@echo off
+mode con: cols=1000 lines=50
+color 0a
+:loop
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%%random% %random% %random% %random% %random% %random%%random% %random% 
+goto loop
